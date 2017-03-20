c9db564959bdd366d546fe06a15eb5f2af826d38
张裕 <zhangyuwinnie@126.com>
2017-03-20 17:11:21 +0000

    complete part2

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -53,6 +53,16 @@ class MoviesController < ApplicationController
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
+  
+  def search
+    @similar_movies = Movie.similar_movies(params[:title])
+    if @similar_movies.nil?
+      redirect_to movies_path
+      flash[:notice] = "'#{params[:title]}' has no director info"
+    end
+    @movie = Movie.find_by(title: params[:title])
+  end
+
 
   def destroy
     @movie = Movie.find(params[:id])
